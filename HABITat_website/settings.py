27ba16dc135b44aebdc8d5b4ba1a42d4245fb40d
JungminHa2000 <Jungmin.Jamie.ha@gmail.com>
2023-10-16 10:41:30 -0600

    Update settings.py

--- HABITat_website/settings.py
+++ HABITat_website/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'HABITat_website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'HABITat_website', 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
