9ead3413f0855246150db4e24901d176a4d16bce
JungminHa2000 <Jungmin.Jamie.ha@gmail.com>
2023-11-06 12:58:08 -0700

    Update urls.py

--- HABITat_website/urls.py
+++ HABITat_website/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import include, path
 from django.views.generic.base import TemplateView
 from . import views
@@ -22,7 +23,7 @@ app_name = 'HABITat_website'
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
-    #path('login/', TemplateView.as_view(template_name='registration/login.html'), name='login'),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('homepage/', TemplateView.as_view(template_name='webHome.html'), name='homepage'),
